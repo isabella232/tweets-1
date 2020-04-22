c5c1fd878c616e09189a9833bc1b12067d7ee1ba
Matthias Koch <ithrowexceptions@gmail.com>
2020-04-22 15:47:26 +0200

    Initialize repository

---
+++ build/Build.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+partial class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Solution] readonly Solution Solution;
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+        });
+}
---
+++ src/shell-completion/shell-completion.md
@@ -0,0 +1,6 @@
+Incomplete without you! 🙃
+
+Update your config files to get shell completion for targets, parameters and their values. Currently supported for Bash, @ohmyzsh, @OhMyFishShell and @PowerShell_Team.
+
+📖 Read more at 
+https://www.nuke.build/docs/running-builds/global-tool.html#shell-snippets 
