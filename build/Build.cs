098efbf263caf620b94964d9e80601de54abd518
Matthias Koch <ithrowexceptions@gmail.com>
2020-04-23 00:09:33 +0200

    asd

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
+using Nuke.Common.Utilities;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.IO.TextTasks;
+using static Nuke.Common.Logger;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
+[GitHubActions(
+    "scheduled",
+    GitHubActionsImage.UbuntuLatest,
+    OnCronSchedule = "*/5 * * * *",
+    InvokedTargets = new[]{nameof(Foo)})]
 partial class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -19,12 +25,40 @@ partial class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Foo);
 
-    [Solution] readonly Solution Solution;
-
-    Target Clean => _ => _
+    Target Foo => _ => _
         .Executes(() =>
         {
+            Console.WriteLine(DateTime.Now);
+        });
+
+    Target Tweet => _ => _
+        .Executes(async () =>
+        {
+            var credentials = new TwitterCredentials(ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret);
+            var client = new TwitterClient(credentials);
+
+            var directory = RootDirectory / "src" / "shell-completion";
+            var text = ReadAllText(directory.GlobFiles("*.md").Single());
+            var media = directory.GlobFiles("*.png", "*.jpeg", "*.jpg", "*.gif")
+                .Select(async x => await client.Upload.UploadTweetImage(
+                    new UploadTweetImageParameters(ReadAllBytes(x))
+                    {
+                        MediaCategory = x.ToString().EndsWithOrdinalIgnoreCase("gif")
+                            ? MediaCategory.Gif
+                            : MediaCategory.Image
+                    }))
+                .Select(x => x.Result).ToList();
+
+            var tweetParameters = new PublishTweetParameters
+                                         {
+                                             Text = text,
+                                             Medias = media
+                                         };
+
+            var tweet = await client.Tweets.PublishTweet(tweetParameters);
+
+            Info($"Sent tweet: https://twitter.com/{tweet.Id}");
         });
 }
